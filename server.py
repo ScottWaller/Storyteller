76b0f24c0eae9427c080efbd2118d59acbb79127
Scott <walles@deepfield.net>
2020-08-01 01:04:45 -0400

    convert to cli

--- server.py
+++ server.py
@@ -1,49 +1,66 @@
 #!/usr/bin/env python
 
-import json
-import tornado.ioloop
-import tornado.web
-from gtts import gTTS
-import pyttsx
+import argparse
+import pyttsx3
 import random
 import uuid
 import subprocess
 
+MALE = "male"
+FEMALE = "female"
 
-def generate_random_voice():
-    gender = random.choice(['male', 'female'])
-    age = random.randint(20, 65)
-    return pyttsx.voice.Voice(id=uuid.uuid4(), gender=gender, age=age)
+FILENAME = "TEST"
 
+class Speaker(object):
+    """docstring for Speaker"""
+    def __init__(self, name, gender):
+        self.name = name
+        self.gender = gender
+        self.voice = generate_voice(name, gender)
 
-class TextHandler(tornado.web.RequestHandler):
-    def post(self):
-        data = json.loads(self.request.body)
-        text = data.get("text", "Just a Test.")
-
-        # gTTS
-        tts = gTTS(text=text, lang='en')
-        tts.save("post_tts.mp3")
-        subprocess.call("mpg123 ./post_tts.mp3", shell=True)
-
-        #pyttsx
-        engine = pyttsx.init()
-        for sentence in text.split("."):
-            voice = generate_random_voice()
-            print voice
-            engine.setProperty('voice', voice.id)
-            engine.setProperty('rate', random.randint(50,100))
-            engine.say(sentence)
+    def speak(self, text, engine):
+        engine.setProperty('voice', self.voice.id)
+        engine.setProperty('rate', random.randint(50,100))
+        engine.say(text)
+        engine.save_to_file(text, FILENAME)
         engine.runAndWait()
 
 
-def make_app():
-    return tornado.web.Application([
-        (r"/", TextHandler),
-    ])
+def find_speaker(text):
+    return random.choice([Speaker("mary", FEMALE)])
+
+
+def generate_voice(name, gender, age=30):
+    return pyttsx3.voice.Voice(id=name, name=name, gender=gender, age=age)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser("Command Line utility for generating tts with different voices")
+    parser.add_argument("--text")
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    text = args.text
+    sentences = text.split(".")
+
+    #pyttsx3
+    engine = pyttsx3.init()
+    # for sentence in text.split("."):
+    #     voice = generate_random_voice()
+    #     print voice
+    #     engine.setProperty('voice', voice.id)
+    #     engine.setProperty('rate', random.randint(50,100))
+    #     engine.say(sentence)
+    # engine.runAndWait()
+    speakers = set([])
+    for sentence in sentences:
+        speaker = find_speaker(sentence)
+        if speaker not in speakers:
+            speakers.add(speaker)
+
+        speaker.speak(sentence, engine)
 
 
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+    main()
